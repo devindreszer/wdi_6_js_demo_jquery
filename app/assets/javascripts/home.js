15c864a8be5df48c4db91518626206681fefbda1
Alex Grant <grantovich@users.noreply.github.com>
2014-07-16 13:49:22 -0400

    Finished demo work

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
   // Code in here will run once the browser has loaded all the page HTML
+  $('#click-me-button').on('click', function(){
+    alert('You did it!');
+  });
+
+  $('#random-color-button').on('click', DemoApp.randomColor);
+  $('.color-changing').on('click', DemoApp.randomColor);
+  $('#disorient').on('click', DemoApp.mirrorElement);
+  $('#color-form').on('submit', DemoApp.addNewColor);
+  $('#colors-list').on('click', 'li', function(){ alert($(this).text()); });
 });
 
 // Create a "namespace" object so we only have a single global variable
 var DemoApp = {
-  testFunction: function() { return true; }
+  randomColor: function(){
+    var random_color = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
+    $(this).css('color', random_color);
+  },
+
+  mirrorElement: function(){
+    $(this).toggleClass('mirror');
+  },
+
+  addNewColor: function(event){
+    var newColor = $('#color-field').val();
+
+    if(newColor !== ''){
+      var newItem = $('<li>').text(newColor).css('color', newColor);
+      $('#colors-list').append(newItem);
+      $('#color-field').val('');
+    }
+
+    event.preventDefault();
+  }
 };
