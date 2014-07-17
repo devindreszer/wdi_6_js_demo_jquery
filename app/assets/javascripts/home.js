c446680493bd30b87cad7e1450502f0c2c524536
Devin Dreszer <devindreszer@gmail.com>
2014-07-17 17:30:04 -0400

    Home page has interactions

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,43 @@
-$(document).ready(function() {
-  // Code in here will run once the browser has loaded all the page HTML
-});
-
-// Create a "namespace" object so we only have a single global variable
 var DemoApp = {
-  testFunction: function() { return true; }
+  initialize: function() {
+    $('#click-me-button').click(function(event) {
+      alert('You did it!');
+      event.preventDefault();
+    });
+
+    $('#disorient').click(this.mirrorText);
+    $('#random-color-button, .color-changing').click(this.randomizeColor);
+    $('form').submit(this.addInput);
+    $('#words-list').on('click', 'li', this.randomizeColor);
+    $('#randomize-words-button').click(this.randomizeAll);
+  },
+
+  mirrorText: function() {
+    $(this).toggleClass('mirror');
+  },
+
+  randomizeColor: function(event) {
+    var newColor = Math.floor(Math.random() * 0xffffff).toString(16);
+    $(this).css('color', '#' + newColor);
+    if(event.preventDefault !== undefined){ event.preventDefault(); }
+  },
+
+  addInput: function(event) {
+    var newWord = $('#word-field').val();
+
+    if(newWord !== '') {
+      var listItem = $('<li>').text(newWord);
+      $('#words-list').append(listItem);
+      $('#word-field').val('');
+    }
+
+    event.preventDefault();
+  },
+
+  randomizeAll: function(event) {
+    $('#words-list li').each(DemoApp.randomizeColor);
+    event.preventDefault();
+  }
 };
+
+$(document).ready(function(){ DemoApp.initialize(); });
